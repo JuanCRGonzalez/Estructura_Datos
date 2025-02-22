51a44ca6dd524f3d6ffe05018663a7d258d4e254
JuanCRGonzalez <juancrgonzalez22@gmail.com>
2025-02-22 14:45:34 +0000

    Tarea_1

---
+++ Tarea_1/multiplicacion.py
@@ -0,0 +1,9 @@
+def multiplicar_suma():
+    a = int(input("Escriba el primer numero: "))
+    b = int(input("Escriba el segundo numero: ")) 
+    resultado_suma = 0
+    for i in range(b):
+        resultado = resultado + a
+    print("La multiplicación es igual a :", resultado)
+
+multiplicar_suma()
